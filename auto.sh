8137517bda86db9815871e66452a251f5f71b0a4
sovlookup <805408477@qq.com>
2020-11-26 18:37:59 +0800

    2020-11-26 18:37:50

--- auto.sh
+++ auto.sh
@@ -3,7 +3,7 @@
 branch_name=$(git symbolic-ref --short -q HEAD)
 commit=$(date "+%Y-%m-%d %H:%M:%S")
 
-echo | git pull
+echo | git pull --ff-only
 echo | ./convert
 echo | git add .
 echo | git commit -m "$commit"
